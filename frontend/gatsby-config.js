5620e00ba24fa84a2676c285a61947b2e29bf5a8
Andrew Stantron <andrew@toddprod.com>
2019-10-31 07:55:32 -0400

    Updated config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -21,24 +21,24 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        baseUrl: "localhost:8080", //WP_API,
-        protocol: "http",
+        baseUrl: WP_API,
+        protocol: "https",
         hostingWPCOM: false,
         useACF: true,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: GA_TRACKING,
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-plugin-google-tagmanager`,
-    //   options: {
-    //     id: GOOGLE_TAG_KEY,
-    //     includeInDevelopment: true,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: GA_TRACKING,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: GOOGLE_TAG_KEY,
+        includeInDevelopment: true,
+      },
+    },
   ],
 }
